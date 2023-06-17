05968241e20dd195d887c846a52e9e4549fdb0c6
Yleus <phvarus@gmail.com>
2023-06-17 14:18:51 -0300

    dssddsd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+    domains: [
+      'https://lucianalanfredi.vercel.app/',
+    ]
+  },}
 
 module.exports = nextConfig
